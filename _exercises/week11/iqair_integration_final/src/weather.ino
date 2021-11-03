49a01cfabb433d2d1f2c4c797d56537822ba4dd6
reparke <reparke@gmail.com>
2021-11-02 19:02:44 -0700

    wk 11

---
+++ _exercises/week11/iqair_integration_final/src/weather.ino
@@ -0,0 +1,45 @@
+#include "JsonParserGeneratorRK.h"
+JsonParser jsonParser;
+
+/*
+  1) publish an event to "Open_Weather"
+  2) Subscribe to the RESPONSE to "Open_Weather" and ask that func "MYHANDLER" get called
+  3) write MYHANDLER--what do you want to happen when a message comes back
+*/
+
+void myHandler(const char *event, const char *data) {
+    // Part 1 allows for webhook responses to be delivered in multple "chunks";
+    // you don't need to change this
+    int responseIndex = 0;
+    const char *slashOffset = strrchr(event, '/');
+    if (slashOffset) responseIndex = atoi(slashOffset + 1);
+    if (responseIndex == 0) jsonParser.clear();
+    jsonParser.addString(data);
+
+    // Part 2 is where you can parse the actual data; you code goes in the IF
+    if (jsonParser.parse()) {
+      // String test = jsonParser.getReference().key("main").valueString();
+      double temp = jsonParser.getReference().key("main").key("temp").valueDouble();
+      String city = jsonParser.getReference().key("name").valueString();
+
+      double windSpeed = jsonParser.getReference().key("wind").key("speed").valueDouble();
+      String weather = jsonParser.getReference().key("weather").index(0).key("main").valueString();
+
+      //get the wind speed
+      //get weather - main (fn .index())
+      Serial.println("The temperature in " + city + " is " + String (temp));
+      Serial.println("The weather is " + weather + " and the wind speed is " + String(windSpeed));
+    }
+}
+
+void setup() {
+    Particle.subscribe("hook-response/Open_Weather", myHandler, MY_DEVICES);
+    Serial.begin(9600);
+}
+
+void loop() {
+
+  Particle.publish("Open_Weather", "");   //trigger our webhook to get data from openweather
+  delay(10000);
+
+}
