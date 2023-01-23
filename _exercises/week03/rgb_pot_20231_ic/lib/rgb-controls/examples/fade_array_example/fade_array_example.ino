ae961bb41007884e2b5c1cd879da3f9945c9fc46
reparke <reparke@gmail.com>
2023-01-23 12:24:37 -0800

    week 3 notes

---
+++ _exercises/week03/rgb_pot_20231_ic/lib/rgb-controls/examples/fade_array_example/fade_array_example.ino
@@ -0,0 +1,17 @@
+#include "rgb-controls.h"
+using namespace RGBControls;
+
+Led led(D0, D1, D2);
+Color red(255, 0, 0);
+Color green(0, 255, 0);
+Color blue(0, 0, 255);
+
+Color colors[3] = { red, green, blue };
+
+
+void setup() { }
+
+void loop() {
+  // Fade between each color in the array every 3 seconds
+  led.fade(colors, 3, 3000);
+}
---
+++ _exercises/week03/rgb_pot_20231_ic/lib/rgb-controls/examples/fade_example/fade_example.ino
@@ -0,0 +1,14 @@
+#include "rgb-controls.h"
+using namespace RGBControls;
+
+Led led(D0, D1, D2);
+Color red(255, 0, 0);
+Color blue(0, 0, 255);
+
+
+void setup() { }
+
+void loop() {
+  // Fade the light between red and blue every second
+  led.fade(red, blue, 1000);
+}
